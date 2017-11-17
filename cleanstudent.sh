bd3e66eb00fc76632ee7ff39385672532c2aa031
TheRandMan <33759959+TheRandMan@users.noreply.github.com>
2017-11-17 09:28:09 -0800

    Create cleanstudent.sh

---
+++ cleanstudent.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#Remove Chrome Files
+#rm -r /Users/student/Library/Application\ Support/Google/Chrome/Default
+
+#Copy Fresh Chrome Files
+#cp -R /Users/student/Library/Fresh/Default /Users/student/Library/Application\ Support/Google/Chrome/
+
+#Remove all files from desktop
+rm -r /Users/student/Desktop/*
+
+#Remove all files from Documents
+rm -r /Users/student/Documents/*
+
+#Remove all files from Downloads
+rm -r /Users/student/Downloads/*
